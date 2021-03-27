0389341160da9b113ae64dbd57efa4cf6aa0bd08
Abhishek <a4abhishekkmr@gmail.com>
2021-03-28 02:10:50 +0530

    till 28th March

---
+++ Section 5 CRUD commmands/1. Introduction to CRUD.sql
@@ -0,0 +1,5 @@
+What is CRUD?
+Create, Read, Update, and Delete (CRUD) are the four basic functions that models should be able to do, at most.
+
+CODE: Introduction to CRUD
+	INSERT INTO cats(name, age) VALUES(‘Taco’, 14); 
---
+++ Section 5 CRUD commmands/7. Update command.sql
@@ -0,0 +1,10 @@
+CODE: Updating Data
+
+
+Change tabby cats to shorthair:
+
+UPDATE cats SET breed='Shorthair' WHERE breed='Tabby'; 
+
+Another update:
+
+UPDATE cats SET age=14 WHERE name='Misty';
