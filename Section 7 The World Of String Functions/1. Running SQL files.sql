6875f04bd87dd662a5bf3b3b03dd374a19c209ea
Abhishek <a4abhishekkmr@gmail.com>
2021-03-31 16:33:08 +0530

    31st march

---
+++ Section 7 The World Of String Functions/1. Running SQL files.sql
@@ -0,0 +1,15 @@
+mysql-ctl cli
+ 
+use cat_app;
+ 
+source first_file.sql
+ 
+DESC cats;
+
+
+
+
+
+
+
+source testing/insert.sql
---
+++ Section 7 The World Of String Functions/2. Loading our book data.sql
@@ -0,0 +1,9 @@
+1. First create your book_data.sql file then
+
+source that file
+source book_data.sql 
+
+3. Now check your work:
+
+DESC books;
+SELECT * FROM books; 
