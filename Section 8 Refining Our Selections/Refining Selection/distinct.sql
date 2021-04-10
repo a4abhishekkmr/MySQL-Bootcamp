57ab009aa24fd7072a26d3d00a5e3a567f218b6d
Abhishek <a4abhishekkmr@gmail.com>
2021-04-10 15:49:27 +0530

    10th April

---
+++ Section 8 Refining Our Selections/Refining Selection/distinct.sql
@@ -0,0 +1,2 @@
+# SELECT CONCAT(Author_fname,' ',Author_lname) from books;
+SELECT DISTINCT     author_fname,author_lname from books;
---
+++ Section 8 Refining Our Selections/Refining Selection/limit.sql
@@ -0,0 +1,2 @@
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 5;
